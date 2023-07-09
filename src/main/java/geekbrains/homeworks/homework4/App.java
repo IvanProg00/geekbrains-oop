04c581689c44de403a51f4be0eed5af2063b8f6d
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-07-09 17:28:08 +0200

    Implement 1 & 2 tasks from homework 4

--- src/main/java/geekbrains/homeworks/homework4/App.java
+++ src/main/java/geekbrains/homeworks/homework4/App.java
@@ -7,15 +7,16 @@ public class App {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
 
-        List<Double> doubleList = new ArrayList<>();
-        List<Integer> integerList = new ArrayList<>();
-
-        for (int i = 0; i < 100; i++) {
-            doubleList.add((double) i);
-            integerList.add(i * 3);
-        }
+        List<Double> doubleList = new ArrayList<>(List.of(5., -4., 3.));
+        List<Integer> integerList = new ArrayList<>(List.of(7, 3, 2));
 
         System.out.println("calc.sum(doubleList) = " + calc.sum(doubleList));
         System.out.println("calc.sum(integerList) = " + calc.sum(integerList));
+
+        System.out.println("calc.multiplication(doubleList) = " + calc.multiplication(doubleList));
+        System.out.println("calc.multiplication(integerList) = " + calc.multiplication(integerList));
+
+        System.out.println("calc.division(doubleList) = " + calc.division(doubleList));
+        System.out.println("calc.division(integerList) = " + calc.division(integerList));
     }
 }
--- src/main/java/geekbrains/homeworks/homework4/Calculator.java
+++ src/main/java/geekbrains/homeworks/homework4/Calculator.java
@@ -12,4 +12,28 @@ public class Calculator {
 
         return result;
     }
+
+    public Double multiplication(List<? extends Number> numbers) {
+        Double result = 1.;
+
+        for (Number number : numbers) {
+            result *= number.doubleValue();
+        }
+
+        return result;
+    }
+
+    public Double division(List<? extends Number> numbers) {
+        if (numbers.isEmpty()) {
+            return 0.;
+        }
+
+        Double result = numbers.get(0).doubleValue();
+
+        for (int i = 1; i < numbers.size(); i++) {
+            result /= numbers.get(i).doubleValue();
+        }
+
+        return result;
+    }
 }
